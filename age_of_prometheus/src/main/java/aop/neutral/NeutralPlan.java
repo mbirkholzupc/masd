7c373d966aac70acaf7a73cb7ae1229000ffa064
Mike <michael.birkholz@estudiantat.upc.edu>
2022-05-13 15:59:51 +0200

    Add neutral agent code

---
+++ age_of_prometheus/src/main/java/aop/neutral/NeutralPlan.java
@@ -0,0 +1,73 @@
+package aop.neutral;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import aop.MoveAction;
+import jadex.bdiv3x.runtime.Plan;
+import jadex.commons.future.DelegationResultListener;
+import jadex.commons.future.Future;
+import jadex.extension.envsupport.environment.ISpaceAction;
+import jadex.extension.envsupport.environment.ISpaceObject;
+import jadex.extension.envsupport.environment.space2d.Grid2D;
+import jadex.extension.envsupport.environment.space2d.Space2D;
+import jadex.extension.envsupport.math.IVector2;
+
+/**
+ *  The behavior of the neutral agent.
+ */
+public class NeutralPlan extends Plan
+{
+	/**
+	 *  Plan body.
+	 */
+	public void body()
+	{
+		Grid2D	env	= (Grid2D)getBeliefbase().getBelief("env").getFact();
+		ISpaceObject	myself	= (ISpaceObject)getBeliefbase().getBelief("myself").getFact();
+		String	lastdir	= null;
+		
+		while(true)
+		{
+			// Get current position.
+			IVector2	pos	= (IVector2)myself.getProperty(Space2D.PROPERTY_POSITION);
+			
+            // Turn 90 degrees with probability 0.25, otherwise continue moving in same direction.
+            if(lastdir==null || Math.random()>0.75)
+            {
+                if(MoveAction.DIRECTION_LEFT.equals(lastdir) || MoveAction.DIRECTION_RIGHT.equals(lastdir))
+                {
+                    lastdir	= Math.random()>0.5 ? MoveAction.DIRECTION_UP : MoveAction.DIRECTION_DOWN;
+                }
+                else
+                {
+                    lastdir	= Math.random()>0.5 ? MoveAction.DIRECTION_LEFT : MoveAction.DIRECTION_RIGHT;
+                }
+            }
+            
+            // Perform move action.
+            try
+            {
+                Map<String, Object> params = new HashMap<String, Object>();
+                params.put(ISpaceAction.ACTOR_ID, getComponentDescription());
+                params.put(MoveAction.PARAMETER_DIRECTION, lastdir);
+                Future<Void> fut = new Future<Void>();
+                env.performSpaceAction("move", params, new DelegationResultListener<Void>(fut));
+                fut.get();
+            }
+            catch(RuntimeException e)
+            {
+                // Move failed, turn 90 degrees.
+                
+                if(MoveAction.DIRECTION_LEFT.equals(lastdir) || MoveAction.DIRECTION_RIGHT.equals(lastdir))
+                {
+                    lastdir	= Math.random()>0.5 ? MoveAction.DIRECTION_UP : MoveAction.DIRECTION_DOWN;
+                }
+                else
+                {
+                    lastdir	= Math.random()>0.5 ? MoveAction.DIRECTION_LEFT : MoveAction.DIRECTION_RIGHT;
+                }
+            }
+		}
+	}
+}
