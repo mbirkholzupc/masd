1f39023527e89348cc6738b1155e17be232a35e1
FunnyStones <alessandro.grassi1998@gmail.com>
2022-05-30 19:28:43 +0200

    solve grab_resource bug

--- age_of_prometheus/src/main/java/aop/dumbprey/DumbPreyPlan.java
+++ age_of_prometheus/src/main/java/aop/dumbprey/DumbPreyPlan.java
@@ -39,18 +39,15 @@ public class DumbPreyPlan extends Plan
 			IVector2	pos	= (IVector2)myself.getProperty(Space2D.PROPERTY_POSITION);
 
 			ISpaceObject	tree	= (ISpaceObject)getBeliefbase().getBelief("nearest_tree").getFact();
-			System.out.println("test1");
 			boolean isOneStepAway = false;
 			if(tree!=null){
 				isOneStepAway = getManhattanDistance(pos, (IVector2)tree.getProperty(Space2D.PROPERTY_POSITION)) == 1;
 			}
-			System.out.println("test2");
 			if(tree!=null && isOneStepAway)
 			{
 				// Perform eat action.
 				try
 				{
-					System.out.println("test3");
 					Map<String, Object> params = new HashMap<String, Object>();
 					params.put(ISpaceAction.ACTOR_ID, getComponentDescription());
 					System.out.println("tree " + tree);
@@ -58,23 +55,20 @@ public class DumbPreyPlan extends Plan
 					Future<Void> fut = new Future<Void>();
 					env.performSpaceAction("grab", params, new DelegationResultListener<Void>(fut));
 					fut.get();
-					System.out.println("test4");
+					// getBeliefbase().getBelief("nearest_tree").setFact(null);
 				}
 				catch(RuntimeException e)
 				{
-					System.out.println("test5");
 					System.out.println("Grab failed: "+e);
 				}
 			}
 
 			else
 			{
-				// Move towards the food, if any
+				// Move towards the tree, if any
 				if(tree!=null)
 				{
-					System.out.println("test6");
 					String	newdir	= MoveAction.getDirection(env, pos, (IVector2)tree.getProperty(Space2D.PROPERTY_POSITION));
-					System.out.println("test7");
 					if(isOneStepAway){
 						newdir = MoveAction.DIRECTION_NONE;
 					}
@@ -86,24 +80,19 @@ public class DumbPreyPlan extends Plan
 					else
 					{
 						// Tree unreachable.
-						System.out.println("test8");
 						getBeliefbase().getBelief("nearest_tree").setFact(null);
-						System.out.println("test9");
 					}
 				}
 
 				// When no food, turn 90 degrees with probability 0.25, otherwise continue moving in same direction.
 				else if(lastdir==null || Math.random()>0.75)
 				{
-					System.out.println("test10");
 					if(MoveAction.DIRECTION_LEFT.equals(lastdir) || MoveAction.DIRECTION_RIGHT.equals(lastdir))
 					{
-						System.out.println("test11");
 						lastdir	= Math.random()>0.5 ? MoveAction.DIRECTION_UP : MoveAction.DIRECTION_DOWN;
 					}
 					else
 					{
-						System.out.println("test12");
 						lastdir	= Math.random()>0.5 ? MoveAction.DIRECTION_LEFT : MoveAction.DIRECTION_RIGHT;
 					}
 				}
@@ -111,19 +100,15 @@ public class DumbPreyPlan extends Plan
 				// Perform move action.
 				try
 				{
-					System.out.println("test13");
 					Map<String, Object> params = new HashMap<String, Object>();
 					params.put(ISpaceAction.ACTOR_ID, getComponentDescription());
 					params.put(MoveAction.PARAMETER_DIRECTION, lastdir);
 					Future<Void> fut = new Future<Void>();
-					System.out.println("params " + params);
 					env.performSpaceAction("move", params, new DelegationResultListener<Void>(fut));
 					fut.get();
-					System.out.println("test14");
 				}
 				catch(RuntimeException e)
 				{
-					System.out.println("test15");
 					// Move failed, forget about food and turn 90 degrees.
 					getBeliefbase().getBelief("nearest_tree").setFact(null);
 
